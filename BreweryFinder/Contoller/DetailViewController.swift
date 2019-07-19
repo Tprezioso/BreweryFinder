f315214c83830ca45b9c693236578ca146c866d4
Tprezioso <tommyprezioso@gmail.com>
2019-07-19 16:35:55 -0400

    got the map to work with coordinates

--- BreweryFinder/Contoller/DetailViewController.swift
+++ BreweryFinder/Contoller/DetailViewController.swift
@@ -9,19 +9,43 @@
 import UIKit
 import MapKit
 
-class DetailViewController: UIViewController {
+class DetailViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
 
     @IBOutlet weak var mapView: MKMapView!
+    
+    let locationManager = CLLocationManager()
 
     var detailBreweryArray = [String : Any]()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        mapView.delegate = self
+        
         setTitleForNavBar()
         print(detailBreweryArray)
+        
+        var lat = ""
+        var longg = ""
+        
+        lat = (detailBreweryArray["latitude"] as! String)
+        longg = (detailBreweryArray["longitude"] as! String)
+        
+        print(lat)
+        
+        let initialLocation = CLLocation(latitude: Double(lat) as! CLLocationDegrees, longitude: Double(longg) as! CLLocationDegrees)
+
+        centerMapOnLocation(location: initialLocation)
+    }
+    
+    let regionRadius: CLLocationDistance = 1000
+    func centerMapOnLocation(location: CLLocation) {
+        let coordinateRegion = MKCoordinateRegion(center: location.coordinate,
+                                                  latitudinalMeters: regionRadius, longitudinalMeters: regionRadius)
+        mapView.setRegion(coordinateRegion, animated: true)
     }
 
+
+    
     func setTitleForNavBar() {
         navigationItem.largeTitleDisplayMode = .never
         self.navigationItem.title = detailBreweryArray["name"] as? String

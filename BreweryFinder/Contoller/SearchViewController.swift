63381db02e25c10215c4316c37788ee9ed43444b
Tprezioso <tommyprezioso@gmail.com>
2019-08-22 17:50:51 -0400

    added picker view to state search

--- BreweryFinder/Contoller/SearchViewController.swift
+++ BreweryFinder/Contoller/SearchViewController.swift
@@ -9,7 +9,23 @@
 import UIKit
 
 
-class SearchViewController: UIViewController {
+class SearchViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return states.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return states[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        breweryStateTextfield.text = states[row]
+    }
+    
     @IBOutlet weak var breweryNameTextfield: UITextField!
     @IBOutlet weak var breweryCityTextfield: UITextField!
     @IBOutlet weak var breweryStateTextfield: UITextField!
@@ -78,6 +94,10 @@ class SearchViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        var pickerView = UIPickerView()
+        pickerView.delegate = (self as! UIPickerViewDelegate)
+        breweryStateTextfield.inputView = pickerView
+        
         breweryNameTextfield.delegate = self
         breweryCityTextfield.delegate = self
         breweryStateTextfield.delegate = self

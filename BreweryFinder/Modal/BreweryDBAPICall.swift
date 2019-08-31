bb80da5172ae58707ee66ef730582dad36795a5e
Tprezioso <tommyprezioso@gmail.com>
2019-08-31 16:13:44 -0400

    setup for using BreweryDB

---
+++ BreweryFinder/Modal/BreweryDBAPICall.swift
@@ -0,0 +1,21 @@
+//
+//  BreweryDBAPICall.swift
+//  BreweryFinder
+//
+//  Created by Thomas Prezioso on 8/31/19.
+//  Copyright © 2019 Thomas Prezioso. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+class BreweryDBAPICall {
+
+    var sandboxURL = "https://sandbox-api.brewerydb.com/v2/"
+    var sandboxKey = "12c7fa76fb0323886d57b76a2d5d4bf4"
+    
+    func getLoacalBrewey() {
+        <#function body#>
+    }
+
+}

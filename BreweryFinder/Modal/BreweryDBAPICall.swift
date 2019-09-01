56b1e6dde792c7c2a5833fa0f35da10757a19833
Tprezioso <tommyprezioso@gmail.com>
2019-09-01 16:42:37 -0400

    trying to figure out why data in API call isnt coming out right

--- BreweryFinder/Modal/BreweryDBAPICall.swift
+++ BreweryFinder/Modal/BreweryDBAPICall.swift
@@ -17,8 +17,8 @@ class BreweryDBAPICall {
     //https://sandbox-api.brewerydb.com/v2/locations?key=12c7fa76fb0323886d57b76a2d5d4bf4&geo=1&lat=38.875532&lng=-77.007294&radius=30&units=m
     
     func getLoacalBrewey(lat: Double, lng: Double) {
-        let requestURLLocalBreweries = "\(sandboxURL)search/geo/point?lat=\(lat)&lng=\(lng)/?key=\(sandboxKey)"
-print(requestURLLocalBreweries)
+        let requestURLLocalBreweries = "\(sandboxURL)locations?key=\(sandboxKey)&geo=1&lat=\(lat)&lng=\(lng)&radius=30&units=m"
+
         Alamofire.request(requestURLLocalBreweries, method: .get).responseJSON { (response) in
             if response.result.isSuccess {
                 print(response.result.value ?? "Nothing was found")
